891d51bd28907065669f06c21bc2ccfec960fc5e
Jack Spence <jackspence123456@gmail.com>
2023-10-17 11:49:12 +0200

    Created a state var and a func to update it.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
+  // Creating a state variable called "activities" and a function to update it called "setActivities"
+  const [activities, setActivities] = useState([]);
+
+  // Using the "useEffect" hook to execute a side-effect in my component
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+  })
+
   return (
     <div>
       <h1>Reactivities</h1>
