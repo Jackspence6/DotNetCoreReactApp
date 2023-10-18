847af0fc8ead67482cef876b3a7b122628eead34
Jack Spence <jackspence123456@gmail.com>
2023-10-18 08:46:33 +0200

    Added a Details Handler.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -16,7 +16,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }
