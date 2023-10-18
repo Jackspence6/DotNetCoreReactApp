e71fd4e7cd41ea1634bbc743a9b9576e1704cc38
Jack Spence <jackspence123456@gmail.com>
2023-10-18 14:15:51 +0200

    Added an Activity Form to app UI page.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
     activities: Activity[];
@@ -17,6 +18,7 @@ export default function ActivityDashboard({ activities }: Props) {
             <Grid.Column width="6">
                 {activities[0] &&
                     <ActivityDetails activity={activities[0]} />}
+                <ActivityForm />
             </Grid.Column>
         </Grid>
     )
