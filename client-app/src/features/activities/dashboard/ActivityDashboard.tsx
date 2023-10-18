419337b7854b5289f9b583cb2cc3d8d997e12c28
Jack Spence <jackspence123456@gmail.com>
2023-10-18 13:46:13 +0200

    Created an Activity List.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
 
 interface Props {
     activities: Activity[];
@@ -10,16 +11,7 @@ export default function ActivityDashboard({ activities }: Props) {
     return (
         <Grid>
             <Grid.Column width="10">
-                {/* Unordered list that will contain the activities */}
-                <List>
-                    {/* Mapping over the "activities" state variable to generate list items */}
-                    {activities.map(activity => (
-                        // Each list item has a unique "key" prop and displays the activity title
-                        <List.Item key={activity.id}>
-                            {activity.title}
-                        </List.Item>
-                    ))}
-                </List>
+                <ActivityList activities={activities} />
             </Grid.Column>
         </Grid>
     )
