68c5546ea9a8548edaba8d88b4204f382bf51a7a
Jack Spence <jackspence123456@gmail.com>
2023-10-18 16:25:10 +0200

    Added GUID for activity id's.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { v4 as uuid } from 'uuid';
 
 function App() {
   // Creating a state variable called "activities" and a function to update it called "setActivities"
@@ -39,7 +40,7 @@ function App() {
 
   function handleCreateOrEditActivity(activity: Activity) {
     activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, activity]);
+      : setActivities([...activities, { ...activity, id: uuid() }]);
     setEditMode(false);
     setSelectedActivity(activity);
   }
