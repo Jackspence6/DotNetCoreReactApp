f42af1f3c95fe1c611dafbde44e9af55cb94565a
Jack Spence <jackspence123456@gmail.com>
2023-10-18 14:02:23 +0200

    Added a Details View to page.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 
 interface Props {
     activities: Activity[];
@@ -12,6 +13,10 @@ export default function ActivityDashboard({ activities }: Props) {
         <Grid>
             <Grid.Column width="10">
                 <ActivityList activities={activities} />
+            </Grid.Column>
+            <Grid.Column width="6">
+                {activities[0] &&
+                    <ActivityDetails activity={activities[0]} />}
             </Grid.Column>
         </Grid>
     )
