8b728a34b8fe924d3aabedc045ed9b5ae60a2d07
Jack Spence <jackspence123456@gmail.com>
2023-10-18 09:54:42 +0200

    Added Edit handler.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -25,5 +25,15 @@ namespace API.Controllers
             await Mediator.Send(new Create.Command { Activity = activity });
             return Ok();
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+
+            await Mediator.Send(new Edit.Command { Activity = activity });
+
+            return Ok();
+        }
     }
 }
