956f80f38b9acaecdc7b4d5c2f52316bb6570719
Jack Spence <jackspence123456@gmail.com>
2023-10-18 11:53:40 +0200

    Added activity interface to respective elements.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
   // Creating a state variable called "activities" and a function to update it called "setActivities"
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   // Using the "useEffect" hook to execute a side-effect in my component
   useEffect(() => {
-    axios.get('http://localhost:5001/api/activities')
+    axios.get<Activity[]>('http://localhost:5001/api/activities')
       .then(Response => {
         setActivities(Response.data)
       })
@@ -23,7 +24,7 @@ function App() {
       {/* Unordered list that will contain the activities */}
       <List>
         {/* Mapping over the "activities" state variable to generate list items */}
-        {activities.map((activity: any) => (
+        {activities.map(activity => (
           // Each list item has a unique "key" prop and displays the activity title
           <List.Item key={activity.id}>
             {activity.title}
