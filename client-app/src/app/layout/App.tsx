a6e1966bc489b18d75ecc74d29ee6598b2245ece
Jack Spence <jackspence123456@gmail.com>
2023-10-19 15:29:15 +0200

    Added functionality to create a new activity.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,7 +3,6 @@ import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
@@ -14,8 +13,6 @@ function App() {
 
   // Creating a state variable called "activities" and a function to update it called "setActivities"
   const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
   const [submitting, setSubmitting] = useState(false);
 
   // Using the "useEffect" hook to execute a side-effect in my component
@@ -23,26 +20,6 @@ function App() {
     activityStore.loadActivities();
     // Empty Array of dependencies 
   }, [activityStore])
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
 
   function handleDeleteActivity(id: string) {
     setSubmitting(true);
@@ -64,7 +41,6 @@ function App() {
       <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
           activities={activityStore.activities}
-          createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
         />
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,14 +8,12 @@ import { observer } from "mobx-react-lite";
 
 interface Props {
     activities: Activity[];
-    createOrEdit: (Activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
 export default observer(function ActivityDashboard({
     activities,
-    createOrEdit,
     deleteActivity,
     submitting
 }: Props) {
@@ -36,10 +34,7 @@ export default observer(function ActivityDashboard({
                     <ActivityDetails
                     />}
                 {editMode &&
-                    <ActivityForm
-                        createOrEdit={createOrEdit}
-                        submitting={submitting}
-                    />}
+                    <ActivityForm />}
             </Grid.Column>
         </Grid>
     )
