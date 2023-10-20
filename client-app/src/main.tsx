fe4da68e3673aa7befc9f23cab018a7b3688e5df
Jack Spence <jackspence123456@gmail.com>
2023-10-20 14:16:47 +0200

    Installed React Router.

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
-import App from './app/layout/App';
 import { store, StoreContext } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router} />
   </StoreContext.Provider>
 );
