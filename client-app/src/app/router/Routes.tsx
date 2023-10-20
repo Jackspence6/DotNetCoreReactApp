f8930f044a9b442a51269f3e969818c51794e752
Jack Spence <jackspence123456@gmail.com>
2023-10-20 15:36:09 +0200

    Added an edit activity route.

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -14,6 +14,7 @@ export const routes: RouteObject[] = [
             { path: "activities", element: <ActivityDashboard /> },
             { path: "activities/:id", element: <ActivityDetails /> },
             { path: "createActivity", element: <ActivityForm /> },
+            { path: "manage/:id", element: <ActivityForm /> },
         ]
     },
 ]
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,23 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useParams } from "react-router-dom";
+import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
-    const { selectedActivity, createActivity, updateActivity, loading } = activityStore;
-
-    const initialState = selectedActivity ?? {
+    const {
+        selectedActivity,
+        createActivity,
+        updateActivity,
+        loading,
+        loadActivity,
+        loadingInitial
+    } = activityStore;
+    const { id } = useParams();
+    const [activity, setActivity] = useState<Activity>({
         id: "",
         title: "",
         category: "",
@@ -15,9 +25,12 @@ export default observer(function ActivityForm() {
         date: "",
         city: "",
         venue: ""
-    }
+    });
 
-    const [activity, setActivity] = useState(initialState);
+    useEffect(() => {
+        if (id) loadActivity(id)
+            .then(activity => setActivity(activity!));
+    }, [id, loadActivity]);
 
     function handleSubmit() {
         activity.id ? updateActivity(activity) : createActivity(activity);
@@ -27,6 +40,8 @@ export default observer(function ActivityForm() {
         const { name, value } = event.target;
         setActivity({ ...activity, [name]: value });
     }
+
+    if (loadingInitial) return <LoadingComponent content="Loading activity..." />
 
     return (
         <Segment clearing>
