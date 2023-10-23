7f5a970f95534ed4dfb54ae02a6a0987afa26b99
Jack Spence <jackspence123456@gmail.com>
2023-10-23 15:15:01 +0200

    Added styling to HomePage.

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,23 +1,12 @@
-import { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { useStore } from "../../../app/stores/store";
-
 interface Props {
     activity: Activity
 }
 
 export default function ActivityListItem({ activity }: Props) {
-    const { activityStore } = useStore();
-    const { deleteActivity, loading } = activityStore;
 
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
     return (
         <Segment.Group>
             <Segment>
