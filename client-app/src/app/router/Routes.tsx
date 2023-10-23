aea579872a3248f9a7e2b70cf00be89726c66df4
Jack Spence <jackspence123456@gmail.com>
2023-10-23 10:34:45 +0200

    Added a key to the route.

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -13,8 +13,8 @@ export const routes: RouteObject[] = [
             { path: "", element: <HomePage /> },
             { path: "activities", element: <ActivityDashboard /> },
             { path: "activities/:id", element: <ActivityDetails /> },
-            { path: "createActivity", element: <ActivityForm /> },
-            { path: "manage/:id", element: <ActivityForm /> },
+            { path: "createActivity", element: <ActivityForm key="create" /> },
+            { path: "manage/:id", element: <ActivityForm key="manage" /> },
         ]
     },
 ]
