1c3bb5aeb8705fbf3b15c6e9aeb0c9963af948b7
Jack Spence <jackspence123456@gmail.com>
2023-10-24 16:28:16 +0200

    Added error handling for invalid GUID's.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -18,10 +18,13 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
-    const { data, status } = error.response as AxiosResponse;
+    const { data, status, config } = error.response as AxiosResponse;
 
     switch (status) {
         case 400:
+            if (config.method === "get" && Object.prototype.hasOwnProperty.call(data.errors, "id")) {
+                router.navigate("/not-found");
+            }
             if (data.errors) {
                 const modalStateErrors = [];
                 for (const key in data.errors) {
