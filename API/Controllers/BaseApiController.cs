dca197044b1d012669aa1548256a19f013593cb7
Jack Spence <jackspence123456@gmail.com>
2023-10-24 13:21:50 +0200

    Added API error handling to edit and delete.

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -15,6 +15,7 @@ namespace API.Controllers
 
         protected ActionResult HandleResult<T>(Result<T> result)
         {
+            if (result == null) return NotFound();
             if (result.IsSuccess && result.Value != null) return Ok(result.Value);
             if (result.IsSuccess && result.Value == null) return NotFound();
 
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,4 +1,4 @@
-using System;
+using Application.Core;
 using MediatR;
 using Persistence;
 
@@ -6,12 +6,12 @@ namespace Application.Activities
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -19,13 +19,17 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
 
+                if (activity == null) return null;
                 _context.Remove(activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to delete the activity!");
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
