8ea9569f25d582ecb4bf695efba98934b6532b93
Jack Spence <jackspence123456@gmail.com>
2023-10-24 14:59:21 +0200

    Prepared the setup for error handling in Client App.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
@@ -10,6 +11,7 @@ function App() {
   return (
     // The root div for this component
     <>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       {location.pathname === "/" ? <HomePage /> : (
         <>
           {/* Heading for the Reactivities list */}
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
