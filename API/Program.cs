7542fec79fa37217a0413f47f8dc03ada10a13cb
Jack Spence <jackspence123456@gmail.com>
2023-10-24 14:23:32 +0200

    Added some error handling exceptions.

--- API/Program.cs
+++ API/Program.cs
@@ -4,14 +4,13 @@ using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Added services to the container.
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configured the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
