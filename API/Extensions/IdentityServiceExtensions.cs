8fdd4f0fa9f3352fb526495f50a641d11f0c107c
Jack Spence <jackspence123456@gmail.com>
2023-10-27 11:05:25 +0200

    Added functionality to allow user registration.

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -15,6 +15,7 @@ namespace API.Extensions
             services.AddIdentityCore<AppUser>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
+                opt.User.RequireUniqueEmail = true;
             })
             .AddEntityFrameworkStores<DataContext>();
 
