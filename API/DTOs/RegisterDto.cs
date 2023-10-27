729086d777ffc39251fd5f85901e2766963f0d55
Jack Spence <jackspence123456@gmail.com>
2023-10-27 11:15:31 +0200

    Added User registration validation.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(
+            "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$",
+            ErrorMessage = "Password must be complex!"
+        )]
+        public string Password { get; set; }
+
+        [Required]
         public string UserName { get; set; }
     }
 }
