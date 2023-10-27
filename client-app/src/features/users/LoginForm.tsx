8f909cd06a1e3050fafe74e6b134047f97998eca
Jack Spence <jackspence123456@gmail.com>
2023-10-27 15:12:26 +0200

    Added functionality to display errors in the form.

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 
@@ -8,16 +8,28 @@ export default observer(function LoginForm() {
     const { userStore } = useStore();
     return (
         <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={values => userStore.login(values)}
+            initialValues={{ email: "", password: "", error: null }}
+            onSubmit={(values, { setErrors }) => userStore.login(values)
+                .catch(() => setErrors({ error: "Invalid Email or Password!" }))}
         >
-            {({ handleSubmit, isSubmitting }) => (
+            {({ handleSubmit, isSubmitting, errors }) => (
                 <Form
                     className="ui form"
                     onSubmit={handleSubmit}
                     autoComplete="off">
                     <MyTextInput placeholder="Email" name="email" />
                     <MyTextInput placeholder="Password" name="password" type="password" />
+                    <ErrorMessage
+                        name="error"
+                        render={() =>
+                            <Label
+                                style={{ marginBottom: 10 }}
+                                basic
+                                color="red"
+                                content={errors.error}
+                            />
+                        }
+                    />
                     <Button loading={isSubmitting} positive content="Login" type="submit" fluid />
                 </Form>
             )}
